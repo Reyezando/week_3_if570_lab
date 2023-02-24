ea76fa45689911fea1d1b6016ee2a6841153a797
ReynardoE15 <reynardo.etantyo2@gmail.com>
2023-02-24 14:36:24 +0700

    Getting started with filters

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,52 +1,45 @@
-import java.util.* // required import
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
+fun main() {
+    val decorations = listOf(
+        "rock", "pagoda", "plastic plant", "alligator",
+        "flowerpot"
+    )
 
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
+    // eager, creates a new list
+        val eager = decorations.filter { it[0] == 'p' }
+        println("eager: $eager")
+
+    // lazy, will wait until asked to evaluate
+        val filtered = decorations.asSequence().filter { it[0] == 'p' }
+        println("filtered: $filtered")
+
+    // force evaluation of the lazy list
+        val newList = filtered.toList()
+        println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
     }
-}
 
-fun swim(speed: String = "fast") {
-    println("swimming $speed")
-}
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
 
-fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return when {
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else -> false
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
     }
-}
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
 
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println ("Today is $day and the fish eat $food")
-    println("Change water: ${shouldChangeWater(day)}")
-}
-
-fun isTooHot(temperature: Int) = temperature > 30
-fun isDirty(dirty: Int) = dirty > 30
-fun isSunday(day: String) = day == "Sunday"
-
-//fun main(args: Array<String>) {
-//    swim() // uses default speed
-//    swim("slow") // positional argument
-//    swim(speed="turtle-like") // named parameter
-//}
-
-fun main(args: Array<String>) {
-    feedTheFish()
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities) // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
 }
